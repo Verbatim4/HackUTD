e447cbb8d0e82bea15f5d82871afa971598ba653
Shreekar Dittakavi <shreksodyne414@gmail.com>
2024-11-16 18:22:27 -0600

    links

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
-@app.route('/sign-up')
+
+@app.route('/signup')
 def sign_up():
     return render_template('signup.html')
+
+@app.route('/')
 @app.route('/login')
 def login():
     return render_template('login.html')
